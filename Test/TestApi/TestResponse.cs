f814cb13d2ffc1699dbcbda7e0f9ba577bc677ab
Dejniel01 <wujkowskidaniel01@gmail.com>
2023-12-11 20:34:02 +0100

    add test api

---
+++ Test/TestApi/TestResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace TestApi
+{
+    public class TestResponse
+    {
+        public string? Rdb {  get; set; }
+        public string? Issuer {  get; set; }
+        public string? Audience {  get; set; }
+    }
+}
