aabbf613655735ef77d93bbd8451e1bef814350d
Dejniel01 <wujkowskidaniel01@gmail.com>
2023-12-11 22:31:51 +0100

    test

--- Test/TestApi/Program.cs
+++ Test/TestApi/Program.cs
@@ -1,4 +1,6 @@
 
+using System.Reflection;
+
 namespace TestApi
 {
     public class Program
@@ -13,6 +15,8 @@ namespace TestApi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Configuration.AddUserSecrets<Program>();
 
             var app = builder.Build();
 
